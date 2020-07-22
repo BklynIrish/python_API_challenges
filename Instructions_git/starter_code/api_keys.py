02c830d8bfab51626c5693dfeb5df9b94f7e2aed
BklynIrish <bm624@nyu.edu>
2020-07-22 13:26:23 -0400

    added the final.ipynb file entitled WeatherPY-STARTING OVER and deleted unappropriate files

--- Instructions_git/starter_code/api_keys.py
+++ Instructions_git/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0fa2d78562d743fb83f04b84910036ae!"
+weather_api_key = "9dc1a0bf941ca8432869138d960cbd37"
 
 # Google API Key
 g_key = "AIzaSyDM5E7Z-3t6QKwgx47SuchO-wh4l1TuCZg"
